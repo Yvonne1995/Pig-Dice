d888a8807da63bd52e466d08a51a9708a384e60a
Yvonne1995 <ykagondu@gmail.com>
2018-03-02 16:33:12 +0300

    more progress

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".start").click(function() {
+
+    $(".section1").show();
+  });
+});
+
+$(document).ready(function() {
+  $(".save").click(function() {
+
+    $(".game").show();
+  });
+});
