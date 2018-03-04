4d931fd3c367a59f4a642a378c4fe4d1201a2ed2
Yvonne1995 <ykagondu@gmail.com>
2018-03-04 15:25:18 +0300

    java progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,42 @@
 $(document).ready(function() {
   $(".start").click(function() {
 
-    $(".section1").show();
+    $(".section1").toggle();
+
   });
 });
 
 $(document).ready(function() {
   $(".save").click(function() {
 
-    $(".game").show();
+    $(".game").toggle();
   });
 });
+
+// business logic
+function Player(player1, player2) {
+  this.Player1=player1;
+  this.Player2=player2;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#player-details").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedplayer1= $("input#player1").val();
+    var inputtedplayer2= $("input#player2").val();
+
+    var newPlayers = new Player(inputtedPlayer1,inputtedPlayer2);
+
+  })
+})
+// <h1 class="test">78</h1>
+// $(document).ready(function() {
+//   var v = $('.test')
+//   var x = parseInt(v.text()) + 1;
+//   v.text(x)
+//   console.log(x)
+//
+//
+// });
